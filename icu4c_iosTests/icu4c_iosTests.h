6e83eb80db318f0e1b0111efb84ec7b922b7214b
chtian <chtian@anfengde.com>
2013-05-19 20:04:58 +0800

    create xcode framework project

---
+++ icu4c_iosTests/icu4c_iosTests.h
@@ -0,0 +1,13 @@
+//
+//  icu4c_iosTests.h
+//  icu4c_iosTests
+//
+//  Created by chtian on 13-5-19.
+//  Copyright (c) 2013年 chtian. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface icu4c_iosTests : SenTestCase
+
+@end
---
+++ icu4c_iosTests/icu4c_iosTests.m
@@ -0,0 +1,32 @@
+//
+//  icu4c_iosTests.m
+//  icu4c_iosTests
+//
+//  Created by chtian on 13-5-19.
+//  Copyright (c) 2013年 chtian. All rights reserved.
+//
+
+#import "icu4c_iosTests.h"
+
+@implementation icu4c_iosTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in icu4c_iosTests");
+}
+
+@end
